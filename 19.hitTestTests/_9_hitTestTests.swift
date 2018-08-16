ba0b35dd543dd27240105243af286f1323429bd4
Pei <1367890978@qq.com>
2018-08-16 14:32:55 +0800

    Initial Commit

---
+++ 19.hitTestTests/_9_hitTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  _9_hitTestTests.swift
+//  19.hitTestTests
+//
+//  Created by xingshulin on 2018/8/16.
+//  Copyright © 2018年 xingshulin. All rights reserved.
+//
+
+import XCTest
+@testable import _9_hitTest
+
+class _9_hitTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
